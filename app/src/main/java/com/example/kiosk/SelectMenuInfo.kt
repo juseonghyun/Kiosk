5daeac983dd6f5e3fbdcae3d1b68851b117e2eeb
Juseonghyun <wntjdgus99@naver.com>
2024-03-14 20:05:50 +0900

    fun checkoutOpertion 오류 발생 공유해서 해결하기위해 위해 임시 push

--- app/src/main/java/com/example/kiosk/SelectMenuInfo.kt
+++ app/src/main/java/com/example/kiosk/SelectMenuInfo.kt
@@ -4,124 +4,175 @@ class SelectMenuInfo {
 
     fun selectMenuOperation() {
 
-        while (true) {
-            DisplayInfo().displayMain()
-            var menuSelectInput = readLine()!!.toInt()
+        try {
+            while (true) {
+                DisplayInfo().displayMain()
+                var menuSelectInput = readLine()!!.toInt()
 
-            if (menuSelectInput == 0) {
-                break
+                if (menuSelectInput == 0) {
+                    break
 
-            } else if (menuSelectInput == 1) {
-                DisplayInfo().displayBurger()
-                burgersMenuOperation()
+                } else if (menuSelectInput == 1) {
+                    DisplayInfo().displayBurger()
+                    burgersMenuOperation()
 
-            } else if (menuSelectInput == 2) {
-                DisplayInfo().displaySide()
-                sidesMenuOperation()
+                } else if (menuSelectInput == 2) {
+                    DisplayInfo().displaySide()
+                    sidesMenuOperation()
 
-            } else if (menuSelectInput == 3) {
-                DisplayInfo().displayDrink()
-                drinksMenuOperation()
+                } else if (menuSelectInput == 3) {
+                    DisplayInfo().displayDrink()
+                    drinksMenuOperation()
 
-            } else if (menuSelectInput == 4) {
-                if (cartList.isEmpty()){
-                    DisplayInfo().displayCheckoutEmpty()
+                } else if (menuSelectInput == 4) {
+                    if (cartList.isEmpty()){
+                        DisplayInfo().displayCheckoutEmpty()
 
-                } else {
-                    DisplayInfo().displayCheckOut()
+                    } else {
+                        DisplayInfo().displayCheckOut()
 
-                    if (checkoutOperation()){
-                        break
+                        if (checkoutOperation()){
+                            break
+                        }
                     }
+                } else {
+                    DisplayInfo().displayException()
                 }
             }
+
+        }catch (e:NumberFormatException){
+            DisplayInfo().displayException()
+            selectMenuOperation()
         }
     }
 
     fun burgersMenuOperation() {
-        while (true) {
-            var menuSelectInput = readLine()!!.toInt()
+        try {
+            while (true) {
+                var menuSelectInput = readLine()!!.toInt()
 
-            if (menuSelectInput == 0) {
-                break
+                if (menuSelectInput == 0) {
+                    break
 
-            } else {
-                for (i in 0..burgersList.size - 1) {
-                    if (menuSelectInput == burgersList[i].id) {
-                        DisplayInfo().displayOrderCheck(burgersList[i])
-                        checkCart(burgersList[i])
-                        return
-
+                } else if (menuSelectInput == 1 || menuSelectInput == 2){
+                    for (i in 0..burgersList.size - 1) {
+                        if (menuSelectInput == burgersList[i].id) {
+                            DisplayInfo().displayOrderCheck(burgersList[i])
+                            checkCart(burgersList[i])
+                            return
+                        }
                     }
+                } else {
+                    DisplayInfo().displayException()
                 }
             }
+
+        } catch (e:NumberFormatException){
+            DisplayInfo().displayException()
+            burgersMenuOperation()
         }
+
     }
 
     fun sidesMenuOperation() {
-        while (true) {
-            var menuSelectInput = readLine()!!.toInt()
+        try {
+            while (true) {
+                var menuSelectInput = readLine()!!.toInt()
 
-            if (menuSelectInput == 0) {
-                break
+                if (menuSelectInput == 0) {
+                    break
 
-            } else {
-                for (i in 0..sidesList.size - 1) {
-                    if (menuSelectInput == sidesList[i].id) {
-                        DisplayInfo().displayOrderCheck(sidesList[i])
-                        checkCart(sidesList[i])
-                        return
+                } else if (menuSelectInput == 1 || menuSelectInput == 2){
+                    for (i in 0..sidesList.size - 1) {
+                        if (menuSelectInput == sidesList[i].id) {
+                            DisplayInfo().displayOrderCheck(sidesList[i])
+                            checkCart(sidesList[i])
+                            return
+                        }
                     }
+                } else {
+                    DisplayInfo().displayException()
                 }
             }
+
+        }catch (e:NumberFormatException){
+            DisplayInfo().displayException()
+            sidesMenuOperation()
         }
+
     }
 
     fun drinksMenuOperation() {
-        while (true) {
-            var menuSelectInput = readLine()!!.toInt()
+        try {
+            while (true) {
+                var menuSelectInput = readLine()!!.toInt()
 
-            if (menuSelectInput == 0) {
-                break
+                if (menuSelectInput == 0) {
+                    break
 
-            } else {
-                for (i in 0..drinksList.size - 1) {
-                    if (menuSelectInput == drinksList[i].id) {
-                        DisplayInfo().displayOrderCheck(drinksList[i])
-                        checkCart(drinksList[i])
-                        return
+                } else if (menuSelectInput == 1 || menuSelectInput == 2){
+                    for (i in 0..drinksList.size - 1) {
+                        if (menuSelectInput == drinksList[i].id) {
+                            DisplayInfo().displayOrderCheck(drinksList[i])
+                            checkCart(drinksList[i])
+                            return
+                        }
                     }
+                } else {
+                    DisplayInfo().displayException()
                 }
             }
+
+        }catch (e:NumberFormatException){
+            DisplayInfo().displayException()
+            drinksMenuOperation()
         }
+
     }
 
+
     fun checkCart(dataModel: DataModel) {
-        while (true) {
-            var checkCart = readLine()!!.toInt()
-            if (checkCart == 1) {
-                cartList.add(dataModel)
-                DisplayInfo().displayAddCart(dataModel)
-                break
-            } else if (checkCart == 2) {
-                break
+        try {
+            while (true) {
+                var checkCart = readLine()!!.toInt()
+                if (checkCart == 1) {
+                    cartList.add(dataModel)
+                    DisplayInfo().displayAddCart(dataModel)
+                    break
+                } else if (checkCart == 2) {
+                    break
+                } else {
+                    DisplayInfo().displayException()
+                }
             }
+
+        }catch (e:NumberFormatException){
+            DisplayInfo().displayException()
+            checkCart(dataModel)
         }
+
     }
 
     fun checkoutOperation(): Boolean {
-        while (true) {
-            var checkOutInput = readLine()!!.toInt()
+        try {
+            while (true) {
+                var checkOutInput = readLine()!!.toInt()
 
-            if (checkOutInput == 1) {
-                return checkoutMoneyOperation()
+                if (checkOutInput == 1) {
+                    return checkoutMoneyOperation()
 
-            } else if (checkOutInput == 2) {
-                break
+                } else if (checkOutInput == 2) {
+                    break
 
+                } else {
+                    DisplayInfo().displayException()
+                }
             }
-        }
 
+        }catch (e:NumberFormatException) {
+            DisplayInfo().displayException()
+            checkoutOperation() // 오류발생 // try-catch문 말고 문자열 예외처리 방법이있는지 or 왜 지금 문자열후 주문하기누르면 함수가 안끝나는지
+        }
         return false
     }
 
